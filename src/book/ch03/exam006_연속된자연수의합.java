2e7de171c6fe5511b32b49ba35776ea61e1171fa
Park Eunhyeong <silbro@gmail.com>
2023-08-31 18:11:11 +0900

    feat : Upload exam006

---
+++ src/book/ch03/exam006_연속된자연수의합.java
@@ -0,0 +1,28 @@
+package src.book.ch03;
+
+import java.util.Scanner;
+
+public class exam006_연속된자연수의합 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int count = 1;
+    int start_index = 1;
+    int end_index = 1;
+    int sum = 1;
+    while (end_index != N) {     // sum == N : end_index++; sum = sum + end_index; count++;
+      if (sum == N) {
+        count++;
+        end_index++;
+        sum = sum + end_index;
+      } else if (sum > N) {      // sum > N : sum = sum - start_index; start_index++;
+        sum = sum - start_index;
+        start_index++;
+      } else if (sum < N) {      // sum < N : end_index++; sum = sum + end_index;
+        end_index++;
+        sum = sum + end_index;
+      }
+    }
+    System.out.println(count);
+  }
+}
