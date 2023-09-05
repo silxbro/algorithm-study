a70eea9bc3900013444ac740e00e488b07c6c573
Park Eunhyeong <silbro@gmail.com>
2023-09-05 11:14:50 +0900

    feat : Upload exam042

---
+++ src/book/ch07/exam042_최소공배수.java
@@ -0,0 +1,22 @@
+package src.book.ch07;
+
+import java.util.Scanner;
+
+public class exam042_최소공배수 {
+  public static void main(String[] args) throws Exception {
+    Scanner sc = new Scanner(System.in);
+    int testCount = sc.nextInt();
+    for (int test = 0; test < testCount; test++) {
+      int firstValue = sc.nextInt();
+      int secondValue = sc.nextInt();
+      int result = (firstValue * secondValue) / gcdFunc(firstValue, secondValue);
+      System.out.println(result);
+    }
+  }
+  public static int gcdFunc(int value1, int value2) {
+    if (value2 == 0) {
+      return value1;
+    }
+    return gcdFunc(value2, value1 % value2);  // 재귀 함수의 형태로 구현
+  }
+}
