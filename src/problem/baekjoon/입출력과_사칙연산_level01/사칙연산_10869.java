fc854ef5ebd125197ad3c8d4e57b250afc7b7436
ehpark <silbroeh@gmail.com>
2024-09-02 22:33:44 +0900

    10869, 168ms, 17688KB

---
+++ src/problem/baekjoon/입출력과_사칙연산_level01/사칙연산_10869.java
@@ -0,0 +1,18 @@
+package src.problem.baekjoon.입출력과_사칙연산_level01;
+
+import java.util.Scanner;
+
+class 사칙연산_10869 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+        System.out.println(A % B);
+    }
+
+}
