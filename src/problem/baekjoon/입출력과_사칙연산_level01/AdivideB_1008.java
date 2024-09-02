ee51186fa1a5288e77add0ba4e0b9b95b0a89846
ehpark <silbroeh@gmail.com>
2024-09-02 22:29:19 +0900

    1008, 176ms, 17948KB

---
+++ src/problem/baekjoon/입출력과_사칙연산_level01/AdivideB_1008.java
@@ -0,0 +1,14 @@
+package src.problem.baekjoon.입출력과_사칙연산_level01;
+
+import java.util.Scanner;
+
+class AdivideB_1008 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double A = scanner.nextDouble();
+        double B = scanner.nextDouble();
+        System.out.println(A / B);
+    }
+
+}
