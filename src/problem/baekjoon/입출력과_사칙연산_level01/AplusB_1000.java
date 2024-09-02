8f36d37abcc8fdb4022df8b8f9fdf0f237e08df6
ehpark <silbroeh@gmail.com>
2024-09-02 22:10:15 +0900

    1000, 164ms, 17724KB

---
+++ src/problem/baekjoon/입출력과_사칙연산_level01/AplusB_1000.java
@@ -0,0 +1,14 @@
+package src.problem.baekjoon.입출력과_사칙연산_level01;
+
+import java.util.Scanner;
+
+class AplusB_1000 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println(A + B);
+    }
+
+}
