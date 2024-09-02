59ccca627e4e5d17ddd5903e76efb762682c18a6
ehpark <silbroeh@gmail.com>
2024-09-03 08:43:24 +0900

    18108, 168ms, 17708KB

---
+++ src/problem/baekjoon/입출력과_사칙연산_level01/_1998년생인_내가_태국에서는_2541년생_18108.java
@@ -0,0 +1,13 @@
+package src.problem.baekjoon.입출력과_사칙연산_level01;
+
+import java.util.Scanner;
+
+class _1998년생인_내가_태국에서는_2541년생_18108 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int result = scanner.nextInt() - (2541 - 1998);
+        System.out.println(result);
+    }
+
+}
