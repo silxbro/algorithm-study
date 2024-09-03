f5691cb87c7405ae4264b712dcedcffd55fb5dad
ehpark <silbroeh@gmail.com>
2024-09-03 09:22:20 +0900

    10171, 96ms, 13884KB

---
+++ src/problem/baekjoon/입출력과_사칙연산_level01/고양이_10171.java
@@ -0,0 +1,12 @@
+package src.problem.baekjoon.입출력과_사칙연산_level01;
+
+class 고양이_10171 {
+
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+
+}
