023932d4489e0a734b0baf4ee479ae1af153fd17
ehpark <silbroeh@gmail.com>
2024-09-03 09:17:39 +0900

    11382, 168ms, 17648KB

---
+++ src/problem/baekjoon/입출력과_사칙연산_level01/꼬마_정민_11382.java
@@ -0,0 +1,16 @@
+package src.problem.baekjoon.입출력과_사칙연산_level01;
+
+import java.util.Scanner;
+
+class 꼬마_정민_11382 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+        long c = scanner.nextLong();
+
+        System.out.println(a + b + c);
+    }
+
+}
