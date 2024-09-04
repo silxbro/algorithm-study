25634e4da7a0473fcf9d1d75c5c9a1e0448aad02
ehpark <silbroeh@gmail.com>
2024-09-04 17:59:55 +0900

    11654, 100ms, 14156KB

---
+++ src/problem/baekjoon/level05_문자열/N11654_아스키_코드.java
@@ -0,0 +1,11 @@
+package src.problem.baekjoon.level05_문자열;
+
+import java.io.IOException;
+
+class N11654_아스키_코드 {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(System.in.read());
+    }
+
+}
