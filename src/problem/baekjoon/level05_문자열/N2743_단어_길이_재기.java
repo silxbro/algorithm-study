f8093d5826fa1aee41d3882d6f030d1a0f80ab30
ehpark <silbroeh@gmail.com>
2024-09-04 17:47:49 +0900

    2743, 160ms, 17592KB

---
+++ src/problem/baekjoon/level05_문자열/N2743_단어_길이_재기.java
@@ -0,0 +1,13 @@
+package src.problem.baekjoon.level05_문자열;
+
+import java.util.Scanner;
+
+class N2743_단어_길이_재기 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        System.out.println(str.length());
+    }
+
+}
