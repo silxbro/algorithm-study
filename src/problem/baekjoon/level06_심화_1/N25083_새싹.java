06c244b2bad70704fa675f63378d9c89f38a844b
ehpark <silbroeh@gmail.com>
2024-09-05 09:21:26 +0900

    25083, 92ms, 14148KB

---
+++ src/problem/baekjoon/level06_심화_1/N25083_새싹.java
@@ -0,0 +1,14 @@
+package src.problem.baekjoon.level06_심화_1;
+
+class N25083_새싹 {
+
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+
+}
