24ce7ad5dfd26c43a6bc1a7a23761e305dd9d38c
ehpark <silbroeh@gmail.com>
2024-09-07 14:20:23 +0900

    24266, 168ms, 17672KB

---
+++ src/problem/baekjoon/level11_시간_복잡도/N24266_알고리즘_수업_알고리즘의_수행_시간_5.java
@@ -0,0 +1,14 @@
+package src.problem.baekjoon.level11_시간_복잡도;
+
+import java.util.Scanner;
+
+class N24266_알고리즘_수업_알고리즘의_수행_시간_5 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextInt();
+
+        System.out.println(n * n * n);
+        System.out.println(3);
+    }
+}
