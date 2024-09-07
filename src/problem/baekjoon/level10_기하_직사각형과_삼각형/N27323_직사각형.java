f7ee4f2fff1d895c13039d232ad084ab56d4cf18
ehpark <silbroeh@gmail.com>
2024-09-07 11:16:26 +0900

    27323, 172ms, 17708KB

---
+++ src/problem/baekjoon/level10_기하_직사각형과_삼각형/N27323_직사각형.java
@@ -0,0 +1,13 @@
+package src.problem.baekjoon.level10_기하_직사각형과_삼각형;
+
+import java.util.Scanner;
+
+class N27323_직사각형 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println(A * B);
+    }
+}
