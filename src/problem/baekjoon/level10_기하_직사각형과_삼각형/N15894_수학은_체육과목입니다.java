3996a4b4d9bd6a3c1b496c4dbaa45b254d69243d
ehpark <silbroeh@gmail.com>
2024-09-07 12:03:56 +0900

    15894, 168ms, 17660KB

---
+++ src/problem/baekjoon/level10_기하_직사각형과_삼각형/N15894_수학은_체육과목입니다.java
@@ -0,0 +1,13 @@
+package src.problem.baekjoon.level10_기하_직사각형과_삼각형;
+
+import java.util.Scanner;
+
+class N15894_수학은_체육과목입니다 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long N = scanner.nextLong();
+
+        System.out.println(N * 4);
+    }
+}
