6ea616aa25b0c187a6e8a2de5e8a90fbcd1d1d68
ehpark <silbroeh@gmail.com>
2024-09-07 14:08:58 +0900

    24262, 104ms, 14364KB

---
+++ src/problem/baekjoon/level11_시간_복잡도/N24262_알고리즘_수업_알고리즘의_수행_시간_1.java
@@ -0,0 +1,9 @@
+package src.problem.baekjoon.level11_시간_복잡도;
+
+class N24262_알고리즘_수업_알고리즘의_수행_시간_1 {
+
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
