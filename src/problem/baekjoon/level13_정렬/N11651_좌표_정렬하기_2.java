71fc706bdc6251f0a09541f21388288e1c98cb62
ehpark <silbroeh@gmail.com>
2024-09-09 18:18:01 +0900

    11651, 1096ms, 61008KB

---
+++ src/problem/baekjoon/level13_정렬/N11651_좌표_정렬하기_2.java
@@ -0,0 +1,42 @@
+package src.problem.baekjoon.level13_정렬;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+class N11651_좌표_정렬하기_2 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] points = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            points[i][0] = Integer.parseInt(st.nextToken());
+            points[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(points, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
+            }
+        });
+
+        for (int[] point : points) {
+            System.out.println(point[0] + " " + point[1]);
+        }
+    }
+}
