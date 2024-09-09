a897cabf1ed5492e9261f89487afff4fe441e6c6
ehpark <silbroeh@gmail.com>
2024-09-09 17:52:11 +0900

    2751, 4228ms, 171720KB

--- src/problem/baekjoon/level13_정렬/N2751_수_정렬하기_2.java
+++ src/problem/baekjoon/level13_정렬/N2751_수_정렬하기_2.java
@@ -3,22 +3,23 @@ package src.problem.baekjoon.level13_정렬;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 class N2751_수_정렬하기_2 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        List<Integer> list = new ArrayList<>();
+        boolean[] exist = new boolean[2000001];
+
         for (int i = 0; i < N; i++) {
-            list.add(Integer.parseInt(br.readLine()));
+            int number = Integer.parseInt(br.readLine());
+            exist[number + 1000000] = true;
         }
-        Collections.sort(list);
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
+
+        for (int i = 0; i < exist.length; i++) {
+            if (exist[i]) {
+                System.out.println(i - 1000000);
+            }
         }
     }
 }
