f837a45405c493d3229af8cc7edf1ab26ac9de53
ehpark <silbroeh@gmail.com>
2024-09-09 14:06:04 +0900

    2231, 168ms, 17720KB

--- src/problem/baekjoon/level12_브루트_포스/N2231_분해합.java
+++ src/problem/baekjoon/level12_브루트_포스/N2231_분해합.java
@@ -8,9 +8,11 @@ class N2231_분해합 {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
         int answer = 0;
-        for (int n = 1; n <= N; n++) {
+        int start = N - 9 * String.valueOf(N).length(); // 각 자릿수가 모두 9이면서 N의 자릿수와 동일할 때 최솟값
+
+        for (int n = start; n <= N; n++) {
             int num = n;
-            int sum = n;
+            int sum = n;    // 분해합
             while (num > 0) {
                 sum += num % 10;
                 num /= 10;
