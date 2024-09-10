f76e1bca81633b20f12cf25befb259eb951ec8c8
ehpark <silbroeh@gmail.com>
2024-09-10 14:48:25 +0900

    1934, 132ms, 15064KB

---
+++ src/problem/baekjoon/level15_약수_배수와_소수_2/N1934_최소공배수.java
@@ -0,0 +1,36 @@
+package src.problem.baekjoon.level15_약수_배수와_소수_2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+class N1934_최소공배수 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(a * b / gcd(a, b) + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
