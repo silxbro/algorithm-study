d2d7d369ede5c2b75838d8433948545668bf9c73
ehpark <silbroeh@gmail.com>
2024-09-12 16:29:17 +0900

    28278, 700ms, 232364KB

--- src/problem/baekjoon/level16_스택_큐_덱/N28278_스택_2.java
+++ src/problem/baekjoon/level16_스택_큐_덱/N28278_스택_2.java
@@ -1,10 +1,8 @@
 package src.problem.baekjoon.level16_스택_큐_덱;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -13,10 +11,10 @@ class N28278_스택_2 {
     public static void main(String[] args) throws IOException {
         Stack<Integer> stack = new Stack<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
+        StringBuilder stb = new StringBuilder();
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int command = Integer.parseInt(st.nextToken());
@@ -24,6 +22,7 @@ class N28278_스택_2 {
                 stack.push(Integer.parseInt(st.nextToken()));
                 continue;
             }
+
             int result = 0;
             if (command == 2) {
                 result = stack.isEmpty() ? -1 : stack.pop();
@@ -37,11 +36,10 @@ class N28278_스택_2 {
             else {
                 result = stack.isEmpty() ? -1 : stack.peek();
             }
-            bw.write(result + "\n");
+            stb.append(result).append("\n");
         }
+        System.out.println(stb);
 
-        bw.flush();
-        bw.close();
         br.close();
     }
 }
