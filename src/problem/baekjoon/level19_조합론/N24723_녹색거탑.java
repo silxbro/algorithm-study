e9f63275b22e4de2a410cac58afbfb06d4aa3c91
ehpark <silbroeh@gmail.com>
2024-09-15 11:40:10 +0900

    24723, 160ms, 17624KB

---
+++ src/problem/baekjoon/level19_조합론/N24723_녹색거탑.java
@@ -0,0 +1,12 @@
+package src.problem.baekjoon.level19_조합론;
+
+import java.util.Scanner;
+
+class N24723_녹색거탑 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        System.out.println((int)Math.pow(2, N));
+    }
+}
