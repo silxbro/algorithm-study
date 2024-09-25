790b7ef6fa135293a7cc7564f7bff584221eb15a
ehpark <silbroeh@gmail.com>
2024-09-25 11:59:32 +0900

    9663, 5428ms, 17604KB

--- src/problem/baekjoon/level22_백트래킹/N9663_N_Queen.java
+++ src/problem/baekjoon/level22_백트래킹/N9663_N_Queen.java
@@ -1,58 +1,48 @@
 package src.problem.baekjoon.level22_백트래킹;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 class N9663_N_Queen {
     static int count = 0;
     static int N;
+    static int[] position;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         N = scanner.nextInt();
+        position = new int[N + 1];
 
-        for (int i = 1; i <= N; i++) {
-            backtrack(1, new ArrayList<>());
-        }
-
+        backtrack(1);
 
         System.out.println(count);
     }
 
-    private static void backtrack(int row, List<int[]> points) {
+    private static void backtrack(int row) {
         if (row > N) {
             count++;
             return;
         }
         for (int i = 1; i <= N; i++) {
-            int[] now = new int[]{row, i};
-            if (canAdd(now, points)) {
-                List<int[]> added = new ArrayList<>(points);
-                added.add(now);
-                backtrack(row + 1, added);
+            if (canAdd(row, i)) {
+                position[row] = i;
+                backtrack(row + 1);
             }
         }
     }
 
-    private static boolean canAdd(int[] now, List<int[]> points) {
-        if (points.isEmpty()) {
-            return true;
-        }
-        for (int[] point : points) {
-            if (canAttack(now, point)) {
+    private static boolean canAdd(int row, int target) {
+        for (int r = 1; r < row; r++) {
+            if (canAttack(r, position[r], row, target)) {
                 return false;
             }
         }
         return true;
     }
 
-    public static boolean canAttack(int[] point1, int[] point2) {
-        if (point1[1] == point2[1]) {
+    public static boolean canAttack(int row1, int position1, int row2, int position2) {
+        if (position1 == position2) {
             return true;
         }
-        int rowDiff = point1[0] - point2[0];
-        int colDiff = point1[1] - point2[1];
-        return Math.abs(rowDiff) == Math.abs(colDiff);
+        return Math.abs(row1 - row2) == Math.abs(position1 - position2);
     }
 }
