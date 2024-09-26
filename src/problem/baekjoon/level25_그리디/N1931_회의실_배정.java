f61757920f278f7128eb4718a9e0c42dfa8e8739
ehpark <silbroeh@gmail.com>
2024-09-26 15:34:02 +0900

    1931, 452ms, 43956KB

---
+++ src/problem/baekjoon/level25_그리디/N1931_회의실_배정.java
@@ -0,0 +1,44 @@
+package src.problem.baekjoon.level25_그리디;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+class N1931_회의실_배정 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] meeting = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            meeting[i][0] = Integer.parseInt(st.nextToken());
+            meeting[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(meeting, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
+            }
+        });
+
+        int count = 0;
+        int end = -1;
+        for (int i = 0; i < meeting.length; i++) {
+            if (meeting[i][0] >= end) {
+                end = meeting[i][1];
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
