1e5509dba73e6580e6921493daef12d1e6d35351
ehpark <silbroeh@gmail.com>
2024-09-30 10:04:26 +0900

    11053, 124ms, 14452KB

--- src/problem/baekjoon/level23_동적_계획법_1/N11053_가장_긴_증가하는_부분_수열.java
+++ src/problem/baekjoon/level23_동적_계획법_1/N11053_가장_긴_증가하는_부분_수열.java
@@ -1,9 +1,33 @@
 package src.problem.baekjoon.level23_동적_계획법_1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 class N11053_가장_긴_증가하는_부분_수열 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
+        int[] numbers = new int[N + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N + 1];
+        int maxLength = Integer.MIN_VALUE;
+        for (int i = 1; i <= N; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (numbers[j] < numbers[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+
+        System.out.println(maxLength);
     }
-
 }
