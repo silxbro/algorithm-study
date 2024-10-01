24a2894e4cec6ab90b0656316a0629dad1b1a7e8
ehpark <silbroeh@gmail.com>
2024-10-01 10:47:57 +0900

    9251, 212ms, 22012KB

---
+++ src/problem/baekjoon/level23_동적_계획법_1/N9251_LCS.java
@@ -0,0 +1,23 @@
+package src.problem.baekjoon.level23_동적_계획법_1;
+
+import java.util.Scanner;
+
+class N9251_LCS {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str1 = scanner.nextLine();
+        String str2 = scanner.nextLine();
+
+        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
+        for (int i = 1; i <= str1.length(); i++) {
+            for (int j = 1; j <= str2.length(); j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+            }
+        }
+        System.out.println(dp[str1.length()][str2.length()]);
+    }
+}
