524d3b5f7e63e4fdd88e55b5dd8615e11b422e28
ehpark <silbroeh@gmail.com>
2024-10-01 10:50:40 +0900

    주석 추가

--- src/problem/baekjoon/level23_동적_계획법_1/N9251_LCS.java
+++ src/problem/baekjoon/level23_동적_계획법_1/N9251_LCS.java
@@ -9,6 +9,9 @@ class N9251_LCS {
         String str1 = scanner.nextLine();
         String str2 = scanner.nextLine();
 
+        // 처음에 생각한 풀이 : dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1] 중 최댓값을 우선 저장한 후, i와 j의 문자가 동일하면 1 증가
+        // ---> 틀린 방법 : 왜 틀린지 모르겠음
+        // 고친 풀이 : i와 j의 문자가 동일하면 dp[i - 1][j - 1] + 1, 다르면 dp[i - 1][j], dp[i][j - 1] 중 최댓값
         int[][] dp = new int[str1.length() + 1][str2.length() + 1];
         for (int i = 1; i <= str1.length(); i++) {
             for (int j = 1; j <= str2.length(); j++) {
@@ -18,6 +21,7 @@ class N9251_LCS {
                 }
             }
         }
+
         System.out.println(dp[str1.length()][str2.length()]);
     }
 }
