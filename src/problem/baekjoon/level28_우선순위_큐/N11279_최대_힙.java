6f679cdbbcc4adf5a0643b640312f8a7b5e2d001
ehpark <silbroeh@gmail.com>
2024-10-03 19:12:19 +0900

    11279, 280ms, 26248KB

---
+++ src/problem/baekjoon/level28_우선순위_큐/N11279_최대_힙.java
@@ -0,0 +1,27 @@
+package src.problem.baekjoon.level28_우선순위_큐;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class N11279_최대_힙 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        StringBuilder stb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int command = Integer.parseInt(br.readLine());
+            if (command == 0) {
+                stb.append(pq.isEmpty() ? 0 : pq.poll()).append("\n");
+                continue;
+            }
+            pq.add(command);
+        }
+        System.out.println(stb);
+    }
+}
