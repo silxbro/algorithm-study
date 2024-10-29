aa99ddef2f80355507f0725342489a2cbb48a001
ehpark <silbroeh@gmail.com>
2024-10-29 16:39:56 +0900

    4134, 352ms, 14428KB

--- src/problem/baekjoon/level15_약수_배수와_소수_2/N4134_다음_소수.java
+++ src/problem/baekjoon/level15_약수_배수와_소수_2/N4134_다음_소수.java
@@ -1,2 +1,36 @@
-package src.problem.baekjoon.level15_약수_배수와_소수_2;public class N4134_다음_소수 {
+package src.problem.baekjoon.level15_약수_배수와_소수_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class N4134_다음_소수 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long T = Long.parseLong(br.readLine());
+
+        StringBuilder stb = new StringBuilder();
+        for (int t = 0; t < T; t++) {
+            long N = Long.parseLong(br.readLine());
+            while (!isPrime(N)) {
+                N++;
+            }
+            stb.append(N).append("\n");
+        }
+
+        System.out.println(stb);
+    }
+
+    private static boolean isPrime(long num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (long n = 2; n <= Math.sqrt(num); n++) {
+            if (num % n == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
