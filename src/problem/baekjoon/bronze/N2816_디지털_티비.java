9abc10528b6e85fee9e53b3f81b42d87a2a9d3a8
ehpark <silbroeh@gmail.com>
2024-10-30 10:59:32 +0900

    2816, 108ms, 14232KB

---
+++ src/problem/baekjoon/bronze/N2816_디지털_티비.java
@@ -0,0 +1,45 @@
+package src.problem.baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class N2816_디지털_티비 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder stb = new StringBuilder();
+        int kbs1Pointer = 0;
+        int kbs2Pointer = 0;
+
+        for (int i = 0; i < N; i++) {
+            String channel = br.readLine();
+            if (channel.equals("KBS1")) {
+                kbs1Pointer = i;
+            }
+            if (channel.equals("KBS2")) {
+                kbs2Pointer = i;
+            }
+        }
+
+        int pointer;
+        if (kbs1Pointer > kbs2Pointer) {
+            kbs2Pointer++;
+        }
+        for (pointer = 0; pointer < kbs1Pointer; pointer++) {
+            stb.append('1');
+        }
+        for (pointer = kbs1Pointer; pointer > 0; pointer--) {
+            stb.append('4');
+        }
+        for (pointer = 0; pointer < kbs2Pointer; pointer++) {
+            stb.append('1');
+        }
+        for (pointer = kbs2Pointer; pointer > 1; pointer--) {
+            stb.append('4');
+        }
+
+        System.out.println(stb);
+    }
+}
