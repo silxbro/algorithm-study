e823b54c9e63d0ab130b6135eb04271d2b24835a
ehpark <silbroeh@gmail.com>
2024-11-04 12:20:00 +0900

    2164, 228ms, 48948KB

---
+++ src/problem/baekjoon/class02_silver/N2164_카드_2.java
@@ -0,0 +1,25 @@
+package src.problem.baekjoon.class02_silver;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class N2164_카드_2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+
+        System.out.println(queue.poll());
+    }
+}
