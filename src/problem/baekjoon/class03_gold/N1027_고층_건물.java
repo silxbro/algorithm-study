252d263ae0c1bd38aa3cc7e60cf748a7840357bb
ehpark <silbroeh@gmail.com>
2024-11-10 11:25:14 +0900

    refactor

--- src/problem/baekjoon/class03_gold/N1027_고층_건물.java
+++ src/problem/baekjoon/class03_gold/N1027_고층_건물.java
@@ -12,32 +12,28 @@ class N1027_고층_건물 {
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        double[] arr = new double[N];
+        int[] arr = new int[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Double.parseDouble(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] result = new int[N];
+        for (int i = 0; i < N; i++) {
+            double maxInc = Integer.MIN_VALUE;
+            for (int j = i - 1; j >= 0; j--) {
+                double inc = (double)(arr[j] - arr[i]) / (i - j);
+                if (inc > maxInc) {
+                    result[i]++;
+                    result[j]++;
+                    maxInc = inc;
+                }
+            }
         }
 
         int answer = 0;
         for (int i = 0; i < N; i++) {
-            int count = 0;
-            double maxInc = Integer.MIN_VALUE;
-            for (int j = i - 1; j >= 0; j--) {
-                double inc = (arr[j] - arr[i]) / (i - j);
-                if (inc > maxInc) {
-                    maxInc = inc;
-                    count++;
-                }
-            }
-            maxInc = Integer.MIN_VALUE;
-            for (int j = i + 1; j < N; j++) {
-                double inc = (arr[j] - arr[i]) / (j - i);
-                if (inc > maxInc) {
-                    maxInc = inc;
-                    count++;
-                }
-            }
-            answer = Math.max(answer, count);
+            answer = Math.max(answer, result[i]);
         }
         System.out.println(answer);
     }
