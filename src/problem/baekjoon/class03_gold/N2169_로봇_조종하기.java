9df75c46ab4169b2dec0eb2dd70df6781cf0e58f
ehpark <silbroeh@gmail.com>
2024-11-17 18:49:32 +0900

    2169, 556ms, 89340KB

--- src/problem/baekjoon/class03_gold/N2169_로봇_조종하기.java
+++ src/problem/baekjoon/class03_gold/N2169_로봇_조종하기.java
@@ -10,7 +10,6 @@ class N2169_로봇_조종하기 {
     static int N, M;
     static int[][] map;
     static int[][] dp;
-    static int MIN = -100000000;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,30 +25,28 @@ class N2169_로봇_조종하기 {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                dp[i][j] = MIN;
             }
         }
 
         dp[0][0] = map[0][0];
         for (int j = 1; j < M; j++) {
-            dp[0][j] = Math.max(dp[0][j], dp[0][j - 1] + map[0][j]);
+            dp[0][j] = dp[0][j - 1] + map[0][j];
         }
 
         for (int i = 1; i < N; i++) {
             int[] left = new int[M];
             int[] right = new int[M];
 
-            for (int j = 0; j < M; j++) {
-                left[j] = dp[i - 1][j] + map[i][j];
-                right[j] = dp[i - 1][j] + map[i][j];
-            }
+            left[0] = dp[i - 1][0] + map[i][0];
+            right[M - 1] = dp[i - 1][M - 1] + map[i][M - 1];
 
             for (int j = 1; j < M; j++) {
-                left[j] = Math.max(left[j], left[j - 1] + map[i][j]);
+                left[j] = Math.max(dp[i - 1][j], left[j - 1]) + map[i][j];
             }
             for (int j = M - 2; j >= 0; j--) {
-                right[j] = Math.max(right[j], right[j + 1] + map[i][j]);
+                right[j] = Math.max(dp[i - 1][j], right[j + 1])+ map[i][j];
             }
+
             for (int j = 0; j < M; j++) {
                 dp[i][j] = Math.max(left[j], right[j]);
             }
